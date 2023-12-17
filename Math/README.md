e5a082d1319374def47ac1323474891477ae242c
steve <1301637976@qq.com>
2023-12-17 18:55:54 -0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   Math/README.md
    	new file:   Math/sturmLiousville.m

--- Math/README.md
+++ Math/README.md
@@ -13,3 +13,13 @@ calculate the inverse of matrix by gauss-jordan method
 
 ## isExact
 judge the ODE is exact or not
+
+## sturmLiousville
+- tranform the function to sturm-liouville problem:
+ [Differential operator](../../../AppData/Local/Microsoft/Windows/INetCache/IE/5X14QT0A/differential-operators%5B1%5D.html);
+ [Transform from sturm-liouville operator](../../../AppData/Local/Microsoft/Windows/INetCache/IE/JLD6YHZX/4%5B1%5D.01__Sturm-Liouville_Operators);
+ [From a(x)y′′(x)+b(x)y′(x)+c(x)y(x)+λd(x)y(x)=0 to Standard form](../../../AppData/Local/Microsoft/Windows/INetCache/IE/9RZ6G7RH/converting-differential-equation-into-sturm-liouville%5B1%5D);
+- calculate eigenvalue and eigenfunction
+ [example calculate](../../../AppData/Local/Microsoft/Windows/INetCache/IE/JLD6YHZX/13%5B1%5D.02__Sturm-Liouville_Problems)
+- verify the orthogonality
+ theorem 1 @ SEC 11.5
---
+++ Math/sturmLiousville.m
@@ -0,0 +1,11 @@
+%% tranform the function to sturm-liouville problem:
+
+% Differential operator: https://math24.net/differential-operators.html
+% Transform from sturm-liouville operator: https://math.libretexts.org/Bookshelves/Differential_Equations/Introduction_to_Partial_Differential_Equations_(Herman)/04%3A_Sturm-Liouville_Boundary_Value_Problems/4.01%3A_Sturm-Liouville_Operators
+% From a(x)y′′(x)+b(x)y′(x)+c(x)y(x)+λd(x)y(x)=0 to Standard form
+% https://math.stackexchange.com/questions/4578493/converting-differential-equation-into-sturm-liouville#:~:text=This%20transforms%20your%20problem%20in%20y%20to%20a,x%29%20%E2%88%92%2020%203%20f%20%3D%20%CE%BB%20f
+%% calculate eigenvalue and eigenfunction
+% example calculate https://math.libretexts.org/Bookshelves/Differential_Equations/Elementary_Differential_Equations_with_Boundary_Value_Problems_(Trench)/13%3A_Boundary_Value_Problems_for_Second_Order_Linear_Equations/13.02%3A_Sturm-Liouville_Problems
+
+%% verify the orthogonality
+% theorem 1 @ SEC 11.5
